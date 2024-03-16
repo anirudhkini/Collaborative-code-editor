1c39570d5e7be1d691768adfc20fcfc439776739
anirudhkini <33251525+anirudhkini@users.noreply.github.com>
2024-03-17 01:54:04 +0530

    added states for username and password

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import{v4 as uuidv4} from 'uuid';
 
 const Home = () => {
+  const[roomId, setRoomId]=useState('');
+  const[username, setUserName]=useState('');
+  const createNewRoom = (e) =>{
+    e.preventDefault();
+    const id= uuidv4();
+    setRoomId(id);
+  
+  }
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
         <img className="homePageLogo" src="/codeCollab-img.png" alt="code-collab-img" />
         <h4 className='mainLabel'>Paste codeCollab ID</h4>
         <div className='inputGroup'>
-          <input type='text' className='inputBox' placeholder='codeCollab ID'></input>
-          <input type='text' className='inputBox' placeholder='username'></input>
+          <input type='text' className='inputBox' placeholder='codeCollab ID' onChange={(e)=>setRoomId(e.target.value)} value={roomId}></input>
+
+          <input type='text' className='inputBox' placeholder='username' onChange={(e)=> setUserName(e.target.value)} value={username}></input> 
           <button className='btn joinBtn'>Join</button>
           <span className='createInfo'>
             Don't have a codeCollab ID? &nbsp;
-            <a href='' className='createNewBtn'>
+            <a onClick={createNewRoom} href='' className='createNewBtn'>
               create one
             </a>
           </span>

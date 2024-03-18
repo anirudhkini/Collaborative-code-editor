b9ef9b11dd889f3dd23f9fff92cc5740fb4ad635
anirudhkini <33251525+anirudhkini@users.noreply.github.com>
2024-03-19 02:13:54 +0530

    bug fixes

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Client from '../components/Client';
 
 const EditorPage = () => {
+  const [clients,setClients]=useState([
+    {socketId:1, username:'Raj'},
+    {socketId:2, username:'Ram'},
+
+  ]);
   return (
-    <div>Editor Page</div>
-  )
+  <div className='mainWrap'>
+    <div className='aside'>
+      <div className='asideInner'>
+         <div className='logo'>
+            <img className='logoImage' src='/codeCollab-img.png' alt='logo'/>
+         </div>
+         <h3>connected</h3>
+         <div className='clientsList'>
+            {
+              clients.map((client=><Client key={client.socketId} username={client.username}/>))
+            }
+         </div>
+      </div>
+    </div>
+    <div className='editorWrap'>Editor goes here</div>
+  </div>
+  );
 }
 
 export default EditorPage

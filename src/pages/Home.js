c3264f6184058bfdd7669bd6d72e94b7d2f74623
anirudhkini <33251525+anirudhkini@users.noreply.github.com>
2024-03-18 17:49:35 +0530

    redirection to editor page and addition of toast message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,42 @@
 import React, {useState} from 'react';
 import{v4 as uuidv4} from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate= useNavigate();
   const[roomId, setRoomId]=useState('');
   const[username, setUserName]=useState('');
   const createNewRoom = (e) =>{
     e.preventDefault();
     const id= uuidv4();
     setRoomId(id);
-  
+    toast.success('New room is now online!');
+  }
+
+  const joinRoom = () =>{
+    if(!roomId){
+      toast.error('Please enter codeCollab ID')
+      return;
+    }
+    if(!username){
+      toast.error('Please enter a username')
+      return;
+    }
+
+    //Redirect
+    navigate(`/editor/${roomId}`, {
+      state:{
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) =>{
+    console.log('event', e.code);
+    if (e.code=='Enter'){
+      joinRoom();
+    }
   }
   return (
     <div className="homePageWrapper">
@@ -16,10 +44,10 @@ const Home = () => {
         <img className="homePageLogo" src="/codeCollab-img.png" alt="code-collab-img" />
         <h4 className='mainLabel'>Paste codeCollab ID</h4>
         <div className='inputGroup'>
-          <input type='text' className='inputBox' placeholder='codeCollab ID' onChange={(e)=>setRoomId(e.target.value)} value={roomId}></input>
+          <input type='text' className='inputBox' placeholder='codeCollab ID' onChange={(e)=>setRoomId(e.target.value)} value={roomId} onKeyUp={handleInputEnter}></input>
 
-          <input type='text' className='inputBox' placeholder='username' onChange={(e)=> setUserName(e.target.value)} value={username}></input> 
-          <button className='btn joinBtn'>Join</button>
+          <input type='text' className='inputBox' placeholder='username' onChange={(e)=> setUserName(e.target.value)} value={username} onKeyUp={handleInputEnter}></input> 
+          <button className='btn joinBtn' onClick={joinRoom}>Join</button>
           <span className='createInfo'>
             Don't have a codeCollab ID? &nbsp;
             <a onClick={createNewRoom} href='' className='createNewBtn'>
